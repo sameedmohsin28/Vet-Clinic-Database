1df0965a42e5b91caa2f7cb62f5ca9227bdc4900
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-05-24 15:58:05 +0500

    Create animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1, 'Agumon', TO_DATE('2023-05-24', 'YYYY-MM-DD'), 0, false, 10.23);
+INSERT INTO animals VALUES (2, 'Gabumon', TO_DATE('2018-11-15', 'YYYY-MM-DD'), 2, true, 8);
+INSERT INTO animals VALUES (3, 'Pikachu', TO_DATE('2021-01-07', 'YYYY-MM-DD'), 1, false, 15.04);
+INSERT INTO animals VALUES (4, 'Devimon', TO_DATE('2017-05-12', 'YYYY-MM-DD'), 5, true, 11);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+vet_clinic=# CREATE TABLE animals (
+vet_clinic(# id INT NOT NULL    GENERATED ALWAYS AS IDENTITY,
+vet_clinic(# name       TEXT,
+vet_clinic(# date_of_birth      DATE,
+vet_clinic(# escape_attempts    INT,
+vet_clinic(# neutered   BOOLEAN,
+vet_clinic(# weight_kg  DECIMAL(5, 2)
+vet_clinic(# )
+vet_clinic-# ;

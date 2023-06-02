b55b33871fcd88f017c3ecf7e97a36c7b0848f22
cosmosha <cosmoshagan@gmail.com>
2023-06-02 19:54:55 +0000

    Add FK indexes and invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -39,3 +39,26 @@ CREATE TABLE invoices (
   medical_history_id INTEGER NOT NULL,
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
 );
+
+-- create invoice_items table for many-to-many relationship
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price NUMERIC(10,2) NOT NULL,
+  quantity INTEGER NOT NULL,
+  total_price NUMERIC(10,2) NOT NULL,
+  invoice_id INTEGER NOT NULL,
+  treatment_id INTEGER NOT NULL,
+  FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+);
+
+-- create FK indexes
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+CREATE INDEX idx_treatments_medical_histories_treatment_id ON treatments_medical_histories (treatment_id);
+CREATE INDEX idx_treatments_medical_histories_medical_history_id ON treatments_medical_histories (medical_history_id);
+
+
+
